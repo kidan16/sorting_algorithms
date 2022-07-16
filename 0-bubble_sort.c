bf813d8f455abead2ff66175b8e795f70c0959cb
kidan16 <eyueltekle@hotmail.com>
2022-07-16 23:58:35 +0300

    correction

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,7 +8,7 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, temp;
 
 	if (!array || size < 2)
 		return;
@@ -18,7 +18,10 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
 				print_array(array, size);
 			}
 		}
