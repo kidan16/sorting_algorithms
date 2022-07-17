ec641002da36121f311afe0991e0f13a951c5d01
kidan16 <eyueltekle@hotmail.com>
2022-07-17 17:35:03 +0300

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,7 +7,7 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, z, m; min;
+	size_t i, j, min;
 	int temp;
 
 	if (!array || size < 2)
@@ -16,26 +16,10 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-		z = i;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[i])
 				min = j;
-			if (array[j] == array[i])
-			{
-				for (m = i + 1; m < j; m++)
-				{
-					if (array[m] == array[i])
-						continue;
-					else
-					{
-						temp = array[i + 1];
-						array[i + 1] = array[j];
-						array[j] = temp;
-						print_array(array, size);
-					}
-				}
-			}
 		}
 
 		if (min != i)
@@ -45,6 +29,5 @@ void selection_sort(int *array, size_t size)
 			array[i] = temp;
 			print_array(array, size);
 		}
-
 	}
 }
