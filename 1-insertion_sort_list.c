604d77fa97c5e38a13c4faf320b622ff27bf0f1a
kidan16 <eyueltekle@hotmail.com>
2022-07-18 13:07:02 +0300

    correct

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,30 +1,12 @@
 #include "sort.h"
 /**
- * swap - swap the nodes
- * @a: the first node
- * @b: the 2nd node
- */
-
-void swap(listint_t *a, listint_t *b)
-{
-	if (a->prev)
-		a->prev->next = b;
-	if (b->next)
-		b->next->prev = a;
-	a->next = b->next;
-	b->prev = a->prev;
-	a->prev = b;
-	b->next = a;
-}
-
-/**
  * insertion_sort_list - sorts a doubly linkded list.
  * @list: the list to be sorted.
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *reverse, *temp, *head;
+	listint_t *reverse, *a, *b, *temp, *head;
 
 	if (!list)
 		return;
@@ -42,7 +24,18 @@ void insertion_sort_list(listint_t **list)
 		{
 			if (reverse->prev->n > reverse->n)
 			{
-				swap(reverse->prev, reverse);
+				a = reverse->prev;
+				b = reverse;
+
+				if (a->prev)
+					a->prev->next = b;
+				if (b->next)
+					b->next->prev = a;
+
+				a->next = b->next;
+				b->prev = a->prev;
+				a->prev = b;
+				b->next = a;
 				if (!reverse->prev)
 					head = reverse;
 				print_list(head);
