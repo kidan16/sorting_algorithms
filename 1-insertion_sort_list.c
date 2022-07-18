51449cc50d1f222383b14d88868723bcc24c304e
kidan16 <eyueltekle@hotmail.com>
2022-07-18 11:25:15 +0300

    swaping method

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,83 +1,55 @@
 #include "sort.h"
 /**
- * insertion_sort_list - Sorts a doubly linked list in ascending order
- * @list: Size of the doubly linked list.
- * Return: void
+ * swap - swap the nodes
+ * @a: the first node
+ * @b: the 2nd node
+ */
+
+void swap(listint_t *a, listint_t *b)
+{
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
+}
+
+/**
+ * insertion_sort_list - sorts a doubly linkded list.
+ * @list: the list to be sorted.
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *head, *temp, *first, * third, *reverse;
+	listint_t *i, *reverse, *temp, *head;
 
 	if (!list)
 		return;
+
 	head = *list;
 	if (!head || !head->next)
 		return;
+
 	temp = head->next;
-	if (!temp->next)
-	{
-		if ((temp->n) < (temp->prev->n))
-		{
-			head = temp;
-
-			temp->next = head->prev;
-			temp->prev = NULL;
-			head->prev->next = NULL;
-			head->prev->prev = temp;
-		}
-		return;
-	}
-	if (!temp->next->next)
-	{
-		if ((temp->n) < (temp->prev->n))
-		{
-			head = temp;
-
-			temp->next = head->prev;
-			temp->prev = NULL;
-			head->prev->next = head->next;
-			head->prev->prev = temp;
-			head->next->prev = head->prev;
-			print_list(head);
-
-			temp = temp->next;
-		}
-
-		temp = temp->next;
-		if ((temp->n) < (temp->prev->n))
-		{
-			temp->next = head->next;
-			temp->prev = head;
-			head->next->next = NULL;
-			head->next->prev = temp;
-			print_list(head);
-
-			head->next = temp;
-		}
-		return;
-	}
-	while(temp->next->next)
+	while (temp)
 	{
 		reverse = temp;
-		temp = temp->next;
-		while (reverse->prev->prev)
+		while (reverse)
 		{
-			if ((reverse->n) < (reverse->prev->n))
+			if (reverse->prev->n > reverse->n)
 			{
-				third = reverse;
-				first = third->prev->prev;
-
-				third->prev->next = third->next;
-				third->prev->prev = third;
-				first->next = third;
-				third->next->prev = third->prev;
-				third->next = third->prev;
-				third->prev = first;
-				reverse = reverse->next;
-				print_list(head);
+				if (reverse == temp)
+					temp = temp->prev;
+				swap(reverse->prev, reverse);
+				if (!reverse->prev)
+					head = reverse;
+				print_list((const listint_t *)head);
 			}
 			reverse = reverse->prev;
 		}
+		temp = temp->next;
 	}
 }
