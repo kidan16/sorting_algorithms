f38970a690e09dc78d7d87e31a86f7d43e1138b5
kidan16 <eyueltekle@hotmail.com>
2022-07-19 15:11:57 +0300

    correction

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -10,30 +10,26 @@
 void shell_sort(int *array, size_t size)
 {
 	size_t gap = 1, x, z, temp;
-
 	if (!array || size < 2)
 		return;
 
-	while (gap <= size)
+	while (gap < size / 3)
 		gap = gap * 3 + 1;
 
-	while (gap >= 1)
+	while (gap > 0)
 	{
 		for (z = gap; z < size; z++)
 		{
 			for (x = z - gap; x >= 0; x - gap)
 			{
-				if (array[x + gap] > array[x])
-				{
-					break;
-				}
-				else
+				if (array[x + gap] < array[x])
 				{
 					temp = array[x + gap];
 					array[x + gap] = array[x];
 					array[x] = temp;
 				}
-				print_array(array, size);
+				else
+					break;
 
 			}
 		}
