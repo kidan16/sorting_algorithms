94c6343b65d0292f112f35c03cc64696c1e3afed
kidan16 <eyueltekle@hotmail.com>
2022-07-19 23:53:41 +0300

    counting

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -9,19 +9,20 @@
  */
 void counting_sort(int *array, size_t size) 
 {
-	int j, k;
-	int *sort, *array_copy;
-	
+	int k;
+	size_t j;
+	int *sort = NULL, *array_copy = NULL;
+
 	if (!array || size < 2)
 		return;
-	
+
 	k = array[0];
 	for (j = 0; j < size; j++) 
 	{
 		if (array[j] > k)
 		k = array[j];
 	}
-	
+
 	sort = malloc(sizeof(int) * (k + 1));
 	if (!sort)
 		return;
@@ -42,10 +43,7 @@ void counting_sort(int *array, size_t size)
 		return;
 	}
 	for (j = 0; j < size; j++)
-		array_copy[j] = array[j];
-	for (j = 0; j < size; j++)
 		array_copy[--sort[array[j]]] = array[j];
-
 	for (j = 0; j < size; j++)
 		array[j] = array_copy[j];
 
