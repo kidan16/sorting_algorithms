934574d3015265face3c836553b83b2824215a67
kidan16 <eyueltekle@hotmail.com>
2022-07-19 11:13:44 +0300

    correction

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -4,16 +4,15 @@
  * @array: array to sort
  * @start: lowest index of the partition to sort
  * @end: highest index of the partition to sort
- * @size: size
  * Return: index of the partition
  */
-size_t partition(int *array, size_t start, size_t end, size_t size)
+size_t partition(int *array, size_t start, size_t end)
 {
 	size_t j;
 	int temp;
 	size_t partition_index = start;
-	size_t n = sizeof(array) / sizeof(array[0]);
 	int pivot = array[end];
+	size_t n = sizeof(array) / sizeof(array[0]);
 
 	for (j = start + 1; j < end; j++)
 	{
@@ -25,12 +24,12 @@ size_t partition(int *array, size_t start, size_t end, size_t size)
 			partition_index++;
 			print_array(array, n)
 		}
-
-		temp = array[partition_index];
-		array[partition_index] = pivot;
-		pivot = temp;
-		print_array(array, n);
 	}
+
+	temp = array[partition_index];
+	array[partition_index] = pivot;
+	pivot = temp;
+	print_array(array, n);
 	return (partition_index);
 }
 /**
@@ -38,17 +37,16 @@ size_t partition(int *array, size_t start, size_t end, size_t size)
  * @array: array to sort
  * @start: lowest index of the partition
  * @end: highest index of the partition
- * @size: size
  * Return: void
  */
-void sort(int *array, size_t start, size_t end, size_t size)
+void sort(int *array, size_t start, size_t end)
 {
 	if (start > end)
 	{
 		size_t partition_index = partition(array, start, end);
 
 		sort(array, start, partition_index);
-		sort(array, partition_index, end);
+		sort(array, partition_index + 1, end);
 	}
 }
 
@@ -61,5 +59,5 @@ void quick_sort(int *array, size_t size)
 {
 	if (!array || size < 2)
 		return;
-	sort(array, 0, size - 1, size);
+	sort(array, 0, size - 1);
 }
