bbe8cf7145de209d4da50b6e8488e4e39ffdc390
kidan16 <eyueltekle@hotmail.com>
2022-07-19 12:29:29 +0300

    correction

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -11,28 +11,32 @@ size_t partition(int *array, size_t start, size_t end, size_t size)
 {
 	size_t j;
 	int temp;
-	size_t partition_index = start;
+	size_t partition_index = start - 1;
 	int pivot = array[end];
 
-	for (j = start + 1; j < end; j++)
+	for (j = start; j < end; j++)
 	{
 		if (array[j] < pivot)
 		{
-			temp = array[j];
-			array[j] = array[partition_index];
-			array[partition_index] = temp;
 			partition_index++;
-			print_array(array, size)
+			if (partition_index != j)
+			{
+				temp = array[j];
+				array[j] = array[partition_index];
+				array[partition_index] = temp;
+				partition_index++;
+				print_array(array, size)
+			}
 		}
 	}
-	if (pivot < array[partition_index])
+	if (pivot < array[partition_index + 1])
 	{
 		temp = array[partition_index];
 		array[partition_index] = pivot;
 		pivot = temp;
 		print_array(array, size);
 	}
-	return (partition_index);
+	return (partition_index + 1);
 }
 /**
  * sort - sorts a partition of an array of integers.
